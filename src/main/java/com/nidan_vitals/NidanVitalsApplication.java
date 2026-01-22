6d53015dd6f11fd8d0c0349bc26da6cd005dac81
aishajoshi <137846404+aishajoshi@users.noreply.github.com>
2026-01-22 14:55:30 +0545

    Initial project setup

---
+++ src/main/java/com/nidan_vitals/NidanVitalsApplication.java
@@ -0,0 +1,13 @@
+package com.nidan_vitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NidanVitalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NidanVitalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nidan_vitals/NidanVitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nidan_vitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NidanVitalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
